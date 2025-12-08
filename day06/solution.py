a101db57c05e10f7cb2c887dcfea9003142cab27
Rodrigue Alcazar <rodriguealcazar@pm.me>
2025-12-08 19:06:19 +0100

    Solution to day 6, part 1

---
+++ day06/solution.py
@@ -0,0 +1,50 @@
+import argparse
+import re
+from functools import reduce
+from pathlib import Path
+from typing import Literal, TextIO
+
+
+def parse(problems_input: TextIO) -> tuple[list[list[int]], list[str]]:
+    problems = None
+    operators = None
+
+    for line in problems_input:
+        line = line.strip()
+
+        if not line:
+            continue
+
+        parts = re.split(r"\s+", line)
+
+        if not problems:
+            problems = [[] for _ in parts]
+
+        if parts[0] in ("*", "+"):
+            operators = parts
+        else:
+            for i, number in enumerate(parts):
+                problems[i].append(int(number))
+    return problems, operators
+
+
+def problem_result(numbers: list[int], operator: Literal["*", "+"]) -> int:
+    if operator == "+":
+        return sum(numbers)
+    else:
+        return reduce(lambda n, a: n * a, numbers)
+
+
+def main(problems_input: Path):
+    with open(problems_input, "r") as f:
+        problems, operators = parse(f)
+    print(sum([problem_result(p, o) for p, o in zip(problems, operators)]))
+
+        
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input", "-i", type=Path)
+    args = parser.parse_args()
+
+    main(args.input)
---
+++ day06/test_solution.py
@@ -0,0 +1,36 @@
+from io import StringIO
+
+import pytest
+
+from day06.solution import parse, problem_result
+
+
+@pytest.fixture
+def test_input() -> StringIO:
+    return StringIO(
+"""123 328  51 64 
+ 45 64  387 23 
+  6 98  215 314
+*   +   *   + """)
+
+
+@pytest.fixture
+def problems() -> list[list[int]]:
+    return [[123, 45, 6], [328, 64, 98], [51, 387, 215], [64, 23, 314]]
+
+
+@pytest.fixture
+def operators() -> list[str]:
+    return ["*", "+", "*", "+"]
+
+
+def test_parsing(test_input: StringIO, problems: list[list[int]], operators: list[str]):
+    parsed_problems, parsed_operations = parse(test_input)
+
+    assert problems == parsed_problems
+    assert operators == parsed_operations
+
+
+def test_solving_problem():
+    assert problem_result([123, 45, 6], "*") == 33210
+    assert problem_result([328, 64, 98], "+") == 490
