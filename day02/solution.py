505357ad5224925b2b0337532e664b70a012c9a1
Rodrigue Alcazar <rodriguealcazar@pm.me>
2025-12-10 19:50:23 +0100

    Solution for day 2, part 2
    Check for all divisors if we obtain the same sub-string by putting them
    in a set - avoids full string comparison.

--- day02/solution.py
+++ day02/solution.py
@@ -1,4 +1,7 @@
 import argparse
+import math
+from collections.abc import Callable
+from itertools import batched
 from pathlib import Path
 
 
@@ -6,39 +9,79 @@ def parse_ranges(ranges: str) -> list[tuple[str]]:
     return [tuple(r.split("-")) for r in ranges.split(",")]
 
 
-def invalid_in_range(id_range: tuple[str]) -> list[int]:
+def invalids_in_range(id_range: tuple[str], part=1) -> list[int]:
+    is_invalid = VALIDATIONS[part]
     invalids = []
     for n in range(int(id_range[0]), int(id_range[1]) + 1):
-        n = str(n)
-
-        if len(n) % 2 != 0:
-            continue
-
-        if len(n) == 2:
-            if n[0] == n[1]:
-                invalids.append(n)
-            continue
-
-        middle = len(n) // 2
-        if n[:middle] == n[middle:]:
-            invalids.append(n)
-
-    return [int(n) for n in invalids]
-    
-
-def sum_invalid_ids(ranges: list[tuple[str]]) -> int:
-    return sum([i for r in ranges for i in invalid_in_range(r)])
+        if is_invalid(n):
+            invalids.append(int(n))
+    return invalids
 
 
-def main(id_ranges_file: Path):
+def is_repeating_twice(num: int) -> bool:
+    n = str(num)
+
+    if len(n) % 2 != 0:
+        return False
+
+    if len(n) == 2:
+        if n[0] == n[1]:
+            return True
+
+    middle = len(n) // 2
+    if n[:middle] == n[middle:]:
+        return True
+
+    return False
+
+
+def divisors(num: int) -> list[int]:
+    divisors = []
+    # Starting at 1 would include 1 and num itself which we don't need
+    for i in range(2, int(math.sqrt(num)) + 1):
+        if num % i == 0:
+            divisors.append(i)
+
+            div = num // i
+            if i != div:
+                divisors.append(div)
+    return divisors
+
+
+def is_repeating(number: int) -> bool:
+    num = str(number)
+
+    if len(num) < 2:
+        return False
+
+    # Special case if repeating 1 character
+    if len(set(num)) == 1:
+        return True
+
+    for div in divisors(len(num)):
+        chunks = {chunk for chunk in batched(num, len(num) // div)}
+        if len(chunks) == 1:
+            return True
+    return False
+
+
+VALIDATIONS = {1: is_repeating_twice, 2: is_repeating}
+
+
+def sum_invalid_ids(ranges: list[tuple[str]], part=1) -> int:
+    return sum([i for r in ranges for i in invalids_in_range(r, part)])
+
+
+def main(id_ranges_file: Path, part: int):
     with open(id_ranges_file, "r") as f:
         ranges = parse_ranges(f.readline())
-    print(sum_invalid_ids(ranges))
+    print(sum_invalid_ids(ranges, part))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--input", "-i", type=Path)
+    parser.add_argument("--part", "-p", type=int, choices=[1, 2], default=1)
     args = parser.parse_args()
 
-    main(args.input)
+    main(args.input, args.part)
--- day02/test_solution.py
+++ day02/test_solution.py
@@ -1,6 +1,6 @@
 import pytest
 
-from day02.solution import invalid_in_range, parse_ranges, sum_invalid_ids
+from day02.solution import divisors, invalids_in_range, parse_ranges, sum_invalid_ids
 
 
 @pytest.fixture
@@ -14,26 +14,38 @@ def ranges() -> str:
 
 def test_parse_ranges(ranges: str):
     assert [
-        ("11", "22"), 
-        ("95", "115"), 
-        ("998", "1012"), 
-        ("1188511880", "1188511890"), 
+        ("11", "22"),
+        ("95", "115"),
+        ("998", "1012"),
+        ("1188511880", "1188511890"),
         ("222220", "222224"),
-        ("1698522", "1698528"), 
-        ("446443", "446449"), 
-        ("38593856", "38593862"), 
+        ("1698522", "1698528"),
+        ("446443", "446449"),
+        ("38593856", "38593862"),
         ("565653", "565659"),
-        ("824824821", "824824827"), 
-        ("2121212118", "2121212124")
+        ("824824821", "824824827"),
+        ("2121212118", "2121212124"),
     ] == parse_ranges(ranges)
 
 
-def test_invalid_in_range():
-    assert [11, 22] == invalid_in_range(["11", "22"])
-    assert [99] == invalid_in_range(["95", "115"])
-    assert [1010] == invalid_in_range(["998", "1012"])
-    assert [1188511885] == invalid_in_range(["1188511880", "1188511890"])
+def test_invalids_in_range():
+    assert [11, 22] == invalids_in_range(["11", "22"])
+    assert [99] == invalids_in_range(["95", "115"])
+    assert [1010] == invalids_in_range(["998", "1012"])
+    assert [1188511885] == invalids_in_range(["1188511880", "1188511890"])
+
+
+def test_invalids_in_range_part_2():
+    assert [11, 22] == invalids_in_range(["11", "22"], 2)
+    assert [99, 111] == invalids_in_range(["95", "115"], 2)
+    assert [999, 1010] == invalids_in_range(["998", "1012"], 2)
+    assert [1188511885] == invalids_in_range(["1188511880", "1188511890"], 2)
 
 
 def test_sum_invalid_ids(ranges: str):
     assert 1227775554 == sum_invalid_ids(parse_ranges(ranges))
+
+
+def test_divisors():
+    assert [2, 6, 3, 4] == divisors(12)
+    assert [2, 18, 3, 12, 4, 9, 6] == divisors(36)
