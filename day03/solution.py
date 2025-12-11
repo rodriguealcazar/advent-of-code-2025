4dba42779115581ef3f02254e785f7ea856caf48
Rodrigue Alcazar <rodriguealcazar@pm.me>
2025-12-11 19:59:27 +0100

    Solution for day 3, part 2
    Recursively search the max from the current index until there are just
    enough batteries left as missing to total 12.

--- day03/solution.py
+++ day03/solution.py
@@ -1,4 +1,5 @@
 import argparse
+from functools import reduce
 from pathlib import Path
 from typing import TextIO
 
@@ -15,23 +16,49 @@ def largest_joltage(batteries: str) -> int:
     return int(f"{first}{second}")
 
 
-def total_joltage(banks: list[str]) -> int:
-    return sum([largest_joltage(batteries) for batteries in banks])
+def max_at(batteries: list[int]) -> tuple[int, int]:
+    index = -1
+    max = 0
+    for i, n in enumerate(batteries):
+        if n > max:
+            max = n
+            index = i
+    return max, index
+
+
+def largest_joltage_override(batteries: str) -> int:
+    numbers = [int(n) for n in batteries]
+    digits = largest_digit(numbers, [], 12)
+    return reduce(lambda x, y: x * 10 + y, digits)
+
+
+def largest_digit(numbers: list[int], current, left) -> list[int]:
+    if left == 0:
+        return current
+    usable = numbers[:-(left - 1)] if left > 1 else numbers
+    max, index = max_at(usable)
+    return largest_digit(numbers[index + 1 :], current + [max], left - 1)
+
+
+def total_joltage(banks: list[str], part: int = 1) -> int:
+    joltage = largest_joltage if part == 1 else largest_joltage_override
+    return sum([joltage(batteries) for batteries in banks])
 
 
 def parse_banks(banks_input: TextIO) -> list[str]:
     return [l.strip() for l in banks_input]
 
 
-def main(input_path: Path):
+def main(input_path: Path, part: int):
     with open(input_path, "r") as f:
         banks = parse_banks(f)
-    print(total_joltage(banks))
+    print(total_joltage(banks, part))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--input", "-i", type=Path)
+    parser.add_argument("--part", "-p", type=int, choices=[1, 2], default=1)
     args = parser.parse_args()
 
-    main(args.input)
+    main(args.input, args.part)
--- day03/test_solution.py
+++ day03/test_solution.py
@@ -1,6 +1,6 @@
 import pytest
 
-from day03.solution import largest_joltage, total_joltage
+from day03.solution import largest_joltage, largest_joltage_override, total_joltage
 
 
 @pytest.fixture
@@ -13,6 +13,13 @@ def test_largest_joltage():
     assert 89 == largest_joltage("811111111111119")
     assert 78 == largest_joltage("234234234234278")
     assert 92 == largest_joltage("818181911112111")
+
+
+def test_largest_joltage_override():
+    assert 987654321111 == largest_joltage_override("987654321111111")
+    assert 811111111119 == largest_joltage_override("811111111111119")
+    assert 434234234278 == largest_joltage_override("234234234234278")
+    assert 888911112111 == largest_joltage_override("818181911112111")
 
 
 def test_total_joltage(batteries: list[str]):
